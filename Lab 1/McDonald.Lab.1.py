e4d4f825c6a5e40d14a3b6d1fbf116adf50767bf
Cassie McDonald <cmcdonald@olivetcollege.edu>
2012-09-19 11:26:51 -0400

    Lab 1
    Olympic Flag

---
+++ Lab 1/McDonald.Lab.1.py
@@ -0,0 +1,35 @@
+###### Cassie McDonald
+####### Computer Science II
+####### CS 240
+####### Lab 1: Pygame
+
+import pygame
+
+pygame.init() ##Initialized pygame module
+
+##Set Screen Size
+width, height = 640, 480
+
+screen = pygame.display.set_mode((width, height))
+
+## Make Flag
+
+
+run = True
+while run:
+    screen.fill((0, 191,255)) ## Deep Sky Blue background
+    pygame.draw.rect(screen, [255, 255, 255], [120, 120, 400, 240]) ## Draws flag
+    pygame.draw.circle(screen, [0, 0, 205], [200, 200], 50, 5) ## Draws First Circle - Blue
+    pygame.draw.circle(screen, [255, 255, 0], [255, 260], 50, 5) ## Draws Second Circle - Yellow
+    pygame.draw.circle(screen, [0, 0, 0], [310, 200], 50, 5) ## Draws Third Circle - Black
+    pygame.draw.circle(screen, [0, 100, 0], [365, 260], 50, 5) ## Draws Fourth Circle - Green
+    pygame.draw.circle(screen, [255, 0, 0], [415, 200], 50, 5) ## Draws Fifth Circle - Red
+    pygame.display.flip() ##Flips to the surface
+
+
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+            run = False ##exits program by hitting the 'x' or pushing q
+            
