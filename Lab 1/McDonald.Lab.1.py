23256a401c8a59d0890210f9335dfa4521a1d75a
Cassie McDonald <cmcdonald@olivetcollege.edu>
2012-09-20 12:04:43 -0400

    defined colors
    defined colors ahead of time to make program more readable

--- Lab 1/McDonald.Lab.1.py
+++ Lab 1/McDonald.Lab.1.py
@@ -17,17 +17,25 @@ screen = pygame.display.set_mode((width, height))
 flag_width, flag_height = 360, 240
 flag = pygame.Rect((width - flag_width)/2, (height - flag_height)/2, flag_width, flag_height)
 
+## Colors of Rings and Flag
+white = pygame.Color(255, 255, 255)
+blue = pygame.Color(0, 0, 205)
+yellow = pygame.Color(255, 255, 0)
+black = pygame.Color(0, 0, 0)
+green = pygame.Color(0, 100, 0)
+red = pygame.Color(255, 0, 0)
+
 ## Main Game Loop, continues until user exits
 run = True
 while run:
     ## Draws an Olympic Flag on a light blue background
     screen.fill((0, 191,255))                                       ## Deep Sky Blue background
-    pygame.draw.rect(screen, (255, 255, 255), flag)                 ## Draws flag
-    pygame.draw.circle(screen, (0, 0, 205), (200, 200), 50, 5)      ## Draws First Circle - Blue
-    pygame.draw.circle(screen, (255, 255, 0), (255, 260), 50, 5)    ## Draws Second Circle - Yellow
-    pygame.draw.circle(screen, (0, 0, 0), (310, 200), 50, 5)        ## Draws Third Circle - Black
-    pygame.draw.circle(screen, (0, 100, 0), (365, 260), 50, 5)      ## Draws Fourth Circle - Green
-    pygame.draw.circle(screen, (255, 0, 0), (415, 200), 50, 5)      ## Draws Fifth Circle - Red
+    pygame.draw.rect(screen, white, flag)                 ## Draws flag
+    pygame.draw.circle(screen, blue, (200, 200), 50, 5)      ## Draws First Circle - Blue
+    pygame.draw.circle(screen, yellow, (255, 260), 50, 5)    ## Draws Second Circle - Yellow
+    pygame.draw.circle(screen, black, (310, 200), 50, 5)        ## Draws Third Circle - Black
+    pygame.draw.circle(screen, green, (365, 260), 50, 5)      ## Draws Fourth Circle - Green
+    pygame.draw.circle(screen, red, (415, 200), 50, 5)      ## Draws Fifth Circle - Red
 
     ## Flips to the surface
     pygame.display.flip()
