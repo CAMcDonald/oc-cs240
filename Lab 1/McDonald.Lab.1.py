34c7d730f7c6566ee93a1afbe04d6eb522282dd5
Cassie McDonald <cmcdonald@olivetcollege.edu>
2012-09-26 22:00:30 -0400

    Drew image on screen before drawing flag...puts image behind the flag.

--- Lab 1/McDonald.Lab.1.py
+++ Lab 1/McDonald.Lab.1.py
@@ -37,23 +37,16 @@ vertical = 3
 ## Main Game Loop, continues until user exits
 run = True
 while run:
-
+           
     ## Draws an Olympic Flag on a light blue background
     screen.fill((0, 191,255))                                       ## Deep Sky Blue background
+    screen.blit(michael, michael_rect)                              ## Draws image behind flag
     pygame.draw.rect(screen, white, flag)                           ## Draws flag
     pygame.draw.circle(screen, blue, (200, 200), 50, 5)             ## Draws First Circle - Blue
     pygame.draw.circle(screen, yellow, (255, 260), 50, 5)           ## Draws Second Circle - Yellow
     pygame.draw.circle(screen, black, (310, 200), 50, 5)            ## Draws Third Circle - Black
     pygame.draw.circle(screen, green, (365, 260), 50, 5)            ## Draws Fourth Circle - Green
     pygame.draw.circle(screen, red, (415, 200), 50, 5)              ## Draws Fifth Circle - Red
-
-    
-    ## Draws Image in Background
-    screen.blit(michael, michael_rect)
-    rotate
-
-    ## Flips to the surface
-    pygame.display.flip()
 
     ## Rotation and movement of the picture
     michael_rect[0] += horizontal
@@ -67,6 +60,9 @@ while run:
         vertical = -3
     elif michael_rect.top <= 0:
         vertical = 3
+    
+    ## Flips to the surface
+    pygame.display.flip()
 
     ## Exits program by hitting the 'x' or pushing q
     for event in pygame.event.get():
